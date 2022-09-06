b164edc45dcd15ce9394394ef2b2ed1fdb124b1a
pdvrostovSPb <pdvrostov@mail.ru>
2022-09-06 08:29:13 +0300

    added try/except to day_of_the_week

--- HW_Seminar01_Introduction/day_of_the_week.py
+++ HW_Seminar01_Introduction/day_of_the_week.py
@@ -5,12 +5,16 @@
 # - 7 -> да
 # - 1 -> нет
 
-day_of_the_week = int(input('Введите номер дня недели от 1 до 7 '))
-if day_of_the_week == 6 or day_of_the_week == 7:
-    print('да')
-elif day_of_the_week >= 1 and day_of_the_week <= 5:
-    print('Нет')
-else:
+try:
+    day_of_the_week = int(input('Введите номер дня недели от 1 до 7 '))
+    if day_of_the_week == 6 or day_of_the_week == 7:
+        print('да')
+    elif day_of_the_week >= 1 and day_of_the_week <= 5:
+        print('Нет')
+    else:
+        print('Некорректный ввод')
+
+except:
     print('Некорректный ввод')
 
 
