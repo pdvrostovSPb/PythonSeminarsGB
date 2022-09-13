86aa8eb3d76f2197d73c71f57735a0cebe355466
pdvrostovSPb <pdvrostov@mail.ru>
2022-09-13 10:43:46 +0300

    added solution fracrional_part.py

--- HW_Seminar03_function_modules/fracrional_part.py
+++ HW_Seminar03_function_modules/fracrional_part.py
@@ -2,3 +2,9 @@
 # найдёт разницу между максимальным и минимальным значением дробной части элементов.
 # Пример:
 # - [1.1, 1.2, 3.1, 5, 10.01] => 0.19
+
+real_numbers = [1.11, 3.33, 10.00]
+for i in range(len(real_numbers)):
+    real_numbers[i] = real_numbers[i] - round(real_numbers[i])
+print(round(max(real_numbers) - min(real_numbers), 2)) 
+
