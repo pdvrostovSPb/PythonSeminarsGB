dda0af271f5612eb942123ae3365f0e92534edd6
pdvrostovSPb <pdvrostov@mail.ru>
2022-09-13 09:30:56 +0300

    added solution product_of_pairs.py

--- HW_Seminar03_function_modules/product_of_pairs.py
+++ HW_Seminar03_function_modules/product_of_pairs.py
@@ -4,6 +4,11 @@
 # - [2, 3, 4, 5, 6] => [12, 15, 16];
 # - [2, 3, 5, 6] => [12, 15]
 
-data_entry  = [2, 8, 5, 6, 5, 1, 4, 5, 10]
-for i in range(int((len(data_entry)) / 2)):
-    print((data_entry[i] * data_entry[len(data_entry) - 1 - i]))
+data_entry = [2, 8, 5, 6, 30, 1, 4, 5, 1]
+len_data_entry = 0
+if not len(data_entry) % 2:
+    len_data_entry = len(data_entry) / 2
+else:
+    len_data_entry = len(data_entry) / 2 + 1
+for i in range(int(len_data_entry)):
+    print((data_entry[i] * data_entry[len(data_entry) - 1 - i]), end=' ')
