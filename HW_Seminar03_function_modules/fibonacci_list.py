240c8002d8c7d9db10470b2c722f24a0cb86045b
pdvrostovSPb <pdvrostov@mail.ru>
2022-09-14 14:23:13 +0300

    added fibonacci_list.py solution

--- HW_Seminar03_function_modules/fibonacci_list.py
+++ HW_Seminar03_function_modules/fibonacci_list.py
@@ -5,13 +5,29 @@
 # - для k = 8 список будет выглядеть так:
 # [-21 ,13, -8, 5, −3, 2, −1, 1, 0, 1, 1, 2, 3, 5, 8, 13, 21]
 
-from array import array
+def fib(n):
+    if n in [1, 2]:
+        return 1
+    else:
+        return fib(n - 1) + fib(n - 2)
+
+n = int(input("Введите число n = "))
+list = []
+for i in range(1, n + 1):
+    list.append(fib(i))
+
+list_0 = [0]
+list_1 = list.copy()
+list.reverse()
+
+for i in range(0, n - 1, 2):
+    if not len(list) % 2:
+        list[i] = int(list[i]) * -1
+    else:
+        list[i + 1] = int(list[i + 1]) * -1
+
+print(list + list_0 + list_1)
 
 
-n = int(input('Введите число: n = '))
-fibonacci_numbers = array()
 
-for i in range(2, n):
-    fibonacci_numbers[i] = fibonacci_numbers[i - 1] + fibonacci_numbers[i - 2]
-    fibonacci_numbers.append(fibonacci_numbers[i])
     
