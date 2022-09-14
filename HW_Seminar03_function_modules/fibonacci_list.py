c1779f1b80369e0f7752f5ca24788b4ad0586207
pdvrostovSPb <pdvrostov@mail.ru>
2022-09-14 14:30:44 +0300

    added fibonacci exceptions

--- HW_Seminar03_function_modules/fibonacci_list.py
+++ HW_Seminar03_function_modules/fibonacci_list.py
@@ -11,22 +11,29 @@ def fib(n):
     else:
         return fib(n - 1) + fib(n - 2)
 
-n = int(input("Введите число n = "))
-list = []
-for i in range(1, n + 1):
-    list.append(fib(i))
+try:
+    n = int(input("Введите число n = "))
+    list = []
+    for i in range(1, n + 1):
+        list.append(fib(i))
 
-list_0 = [0]
-list_1 = list.copy()
-list.reverse()
+    list_0 = [0]
+    list_1 = list.copy()
+    list.reverse()
 
-for i in range(0, n - 1, 2):
-    if not len(list) % 2:
-        list[i] = int(list[i]) * -1
+    for i in range(0, n - 1, 2):
+        if not len(list) % 2:
+            list[i] = int(list[i]) * -1
+        else:
+            list[i + 1] = int(list[i + 1]) * -1
+
+    if n:
+        print(list + list_0 + list_1)
     else:
-        list[i + 1] = int(list[i + 1]) * -1
+        print('Некорректный ввод')
 
-print(list + list_0 + list_1)
+except:
+    print('Некорректный ввод')
 
 
 
