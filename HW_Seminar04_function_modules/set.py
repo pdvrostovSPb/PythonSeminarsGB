428480c121e8a3e8d3414a01cc0db4d9542c033c
pdvrostovSPb <pdvrostov@mail.ru>
2022-09-15 10:21:51 +0300

    added set.py solution

--- HW_Seminar04_function_modules/set.py
+++ HW_Seminar04_function_modules/set.py
@@ -1,2 +1,18 @@
 # Задайте последовательность чисел. Напишите программу,
 # которая выведет список неповторяющихся элементов исходной последовательности.
+
+list = [2, 45, 7, 6, 2, 12, 45, 1]
+set_list = []
+
+for i in range(len(list)):
+    flag = False
+    for j in range(len(list)):
+        if i == j:
+            continue
+        if list[i] == list[j]:
+            flag = True
+    if flag == False:
+        set_list.append(list[i])
+print(set_list)
+
+
