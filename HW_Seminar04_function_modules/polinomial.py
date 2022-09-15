609cfd24261cdccbc58c63c06a93c009786b3528
pdvrostovSPb <pdvrostov@mail.ru>
2022-09-15 16:02:11 +0300

    added polinominal.py solution

--- HW_Seminar04_function_modules/polinomial.py
+++ HW_Seminar04_function_modules/polinomial.py
@@ -4,3 +4,16 @@
 
 # Пример:
 # - k=2 => 2*x² + 4*x + 5 = 0 или x² + 5 = 0 или 10*x² = 0
+
+from random import randint
+
+
+k = int(input('Введите степень многочлена: k = '))
+coefficients = []
+for i in range(k):
+    if i != k - 1:
+        coefficients.append(str(randint(0, 100)) + 'x' + '^' + str(k - i) + ' + ')
+        print(coefficients[i], end='')
+    else:
+        coefficients.append(str(randint(0, 100)) + 'x' + '^' + str(k - i))
+        print(coefficients[i], '+', randint(0, 100), '= 0')
