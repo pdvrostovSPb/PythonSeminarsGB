6ce6cb7cde9fed62a84ee5a1996a5b515b462666
pdvrostovSPb <pdvrostov@mail.ru>
2022-09-19 18:31:16 +0300

    started sum_polinomials.py

--- HW_Seminar04_function_modules/sum_polinomials.py
+++ HW_Seminar04_function_modules/sum_polinomials.py
@@ -1,9 +1,23 @@
 # 35. Даны два файла, в каждом из которых находится запись многочлена.
 # Задача - сформировать файл, содержащий сумму многочленов.
 
-polinom_1 = open('input_01_polin.txt', 'r')
-print(polinom_1.read().split(' '))
-polinom_1.close()
-polinom_2 = open('input_02_polin.txt', 'r')
-print(polinom_2.read().split(' '))
-polinom_2.close()
+data_entry = open('input_01_polin.txt', 'r')
+polinom_1 = data_entry.read().split(' ')
+data_entry.close()
+data_entry = open('input_02_polin.txt', 'r')
+polinom_2 = data_entry.read().split(' ')
+data_entry.close()
+print(polinom_1)
+print(polinom_2)
+sum = polinom_1 + polinom_2
+print(sum)
+sum_item = 0
+for item in sum:
+    for e in item:
+        if e.find('x'):
+            
+            print(e)
+
+    #     sum_item = sum_item + int(item)
+    #     del(item)
+
