fea5e98a4c8100ddd3abdc48bccaf32f9e132f76
pdvrostovSPb <pdvrostov@mail.ru>
2022-09-21 23:43:33 +0300

    optimized the code in polinomial.py

--- HW_Seminar04_function_modules/polinomial.py
+++ HW_Seminar04_function_modules/polinomial.py
@@ -12,8 +12,8 @@ k = int(input('Введите степень многочлена: k = '))
 coefficients = []
 for i in range(k):
     if i != k - 1:
-        coefficients.append(str(randint(0, 100)) + 'x' + '^' + str(k - i) + ' + ')
+        coefficients.append(str(randint(0, 100)) + '*x^' + str(k - i) + ' + ')
         print(coefficients[i], end='')
     else:
-        coefficients.append(str(randint(0, 100)) + 'x' + '^' + str(k - i))
+        coefficients.append(str(randint(0, 100)) + '*x^' + str(k - i))
         print(coefficients[i], '+', randint(0, 100), '= 0')
