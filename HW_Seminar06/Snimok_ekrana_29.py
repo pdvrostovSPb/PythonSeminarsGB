c0b637cf5f4ed6432ae0c7c750758b0099653c01
pdvrostovSPb <pdvrostov@mail.ru>
2022-09-27 14:08:54 +0300

    finished find_farthest_orbit

--- HW_Seminar06/Snimok_ekrana_29.py
+++ HW_Seminar06/Snimok_ekrana_29.py
@@ -1,16 +1,23 @@
 # Самая далекая планета
 from math import pi
 
-def max_square_index(x):
+def max_square_index(x: list)-> int:
+    '''
+    функция, получающая на вход список кортежей с длинами полуосей эллипсов
+    и возвращающая индекс кортежа с данными самого большого по площади эллипса
+    '''
     square_lst = []
     for i in range(len(x)):
         for j in range(len(x[i]) - 1):
             if x[i][j] == x[i][j + 1]: continue
-            sq = x[i][j] * x[i][j + 1] * pi
-            square_lst.append(sq)
+            square_lst.append(x[i][j] * x[i][j + 1] * pi)
     return square_lst.index(max(square_lst))
 
 def find_farthest_orbit(list_of_orbits)-> tuple:
+    '''
+    Функция возвращающая из списка кортеж с длинами полуосей эллипса
+    с самой большой площадью
+    '''
     return list_of_orbits[max_square_index(list_of_orbits)]
 
 orbits = [(1, 3), (2.5, 10), (7, 2), (6, 6), (4, 3)]
--- Seminar_07/calc.py
+++ Seminar_07/calc.py
@@ -3,18 +3,19 @@
 
 # https://github.com/Naganishe/Calc-Seminar7.git репозиторий 2 зала 7 семинара
 
-
+# Самая далекая планета
 from math import pi
 
+def max_square_index(x):
+    square_lst = []
+    for i in range(len(x)):
+        for j in range(len(x[i]) - 1):
+            if x[i][j] == x[i][j + 1]: continue
+            square_lst.append(x[i][j] * x[i][j + 1] * pi)
+    return square_lst.index(max(square_lst))
+
+def find_farthest_orbit(list_of_orbits)-> tuple:
+    return list_of_orbits[max_square_index(list_of_orbits)]
+
 orbits = [(1, 3), (2.5, 10), (7, 2), (6, 6), (4, 3)]
-lst = [list(i) for i in orbits]
-square_lst = []
-for i in range(len(lst)):
-    for j in range(len(lst[i]) - 1):
-        if lst[i][j] == lst[i][j + 1]: continue
-        sq = lst[i][j] * lst[i][j + 1] * pi
-        square_lst.append(sq)
-    print(square_lst)
-max_sq = max(square_lst)
-index = square_lst.index(max_sq)
-print(orbits[index])
+print(find_farthest_orbit(orbits))
