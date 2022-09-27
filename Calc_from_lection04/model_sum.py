00eb8e530905358bfb139ff43baeac2b91edeb5f
pdvrostovSPb <pdvrostov@mail.ru>
2022-09-27 21:21:31 +0300

    added Calc_from_lection04 solution with model view controller

---
+++ Calc_from_lection04/model_sum.py
@@ -0,0 +1,14 @@
+x = 0
+y = 0
+
+def init(a, b):
+    '''
+    инициализация начальных значений переменных x и y 
+    '''
+    global x
+    global y
+    x = a
+    y = b
+
+def do_it():
+    return x + y
